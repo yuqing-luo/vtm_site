234ff11c30d30f5fef7f7d2eef83c56428842d12
Yuqing Luo <yuqing-luo@uiowa.edu>
2022-04-04 14:45:53 -0500

    vtm_site initial commit

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='VTM Home Page')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html', pageTitle='Estimate')
 
 if __name__ == '__main__':
     app.run(debug=True)
