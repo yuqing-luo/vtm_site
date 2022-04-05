2b7fd03f0e516a35ed26db5a84d0d56deb7fd732
Yuqing Luo <yuqing-luo@uiowa.edu>
2022-04-04 21:11:29 -0500

    Build price estimator

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -11,15 +11,25 @@ def index():
 def about():
     return render_template('about.html', pageTitle='About')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['GET'])
 def estimate():
     return render_template('estimate.html', pageTitle='Estimate')
 
-@app.route('/stylesheet.css')
-def stylesheet():
-    response['Content-type'] = 'text/css'
-
-    return response
+@app.route('/result', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        radius = int(request.form['radius'])
+        height = int(request.form['height'])
+        pi = 3.14
+        top = pi * radius ** 2 
+        side = 2 * 2 * (pi * (radius * height))
+        totalinch = top + side
+        totalsqfeet = totalinch/144
+        material = totalsqfeet * 25
+        labor = totalsqfeet * 15
+        totalcost = "{:.2f}".format(material + labor)
+        print(totalcost)
+    return render_template('estimate.html', totalCost = totalcost)
 
 if __name__ == '__main__':
     app.run(debug=True)
